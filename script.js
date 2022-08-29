3a17de22c42c1e1c7aeb93c15d2050c13f83bb82
Mujahid96 <mujahid919191@gmail.com>
2022-08-29 15:44:35 +0600

    udated

---
+++ script.js
@@ -0,0 +1,21 @@
+// let a=Array.from(document.getElementsByClassName("button"));
+// console.log(a);
+
+// const poslistener =document.getElementById("button1");
+   
+// poslistener.addEventListener('click',function(){
+//     document.getElementById("input_field").innerHTML = "I have changed!";
+//     console.log(poslistener.value)
+  
+
+
+// });
+
+function myFunction(a) {
+    let ans=0;
+    let b= document.getElementById("input_field");
+    b.value+=a;
+   
+
+  
+  }
